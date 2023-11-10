3451b72bebda7f1cee931e198010485cf906fd84
Alec Barr <52548881+FlyingDolphino@users.noreply.github.com>
2023-11-10 17:01:20 +0000

    continued work

--- Fuselage/main.m
+++ Fuselage/main.m
@@ -2,13 +2,25 @@ close all
 clear all
 
 %Geometry
-FuselageRadius = 381;
+%Main Fuselage
+FuselageRadius = 1; %m
 NumberOfStringers = 16; %number disributed around the circumference
-StringerArea  = -100; %mm^2
-SkinThickness = 0.8; %mm 
+StringerArea  = -100e-6; %m^2
+SkinThickness = 2.8e-3; %m 
+
+%Bulkheads
+r = 1.25;%m rear bulkhead radius
+
+NoseLength = 1; %distance from the passenger cabin to the tip of the nose
+a = 2;%m distance from junction with the pax cabin to the tip
+b = FuselageRadius; 
+
 
                     
 %Material Properties
+E = 70e9;
+v = 0.3;
+
 
 %Loads
 
@@ -17,6 +29,7 @@ F = 100e3; %N
 Fd = 0;   %mm, distance the shear force is applied from the vertical line of symmetry
 %this is to be changed to take loads from an excel pending W+B figures
 
+P = 500;
 
 %Boom and Skin idealisation
 %setup positions
@@ -86,4 +99,22 @@ qs = qs+qPureTorsion;
 
 
 %Pressure
+d = 2*FuselageRadius;
+t = SkinThickness;  %reassigning variables for easier coding
 
+longStress = P*d/4*t;
+circumStress = 2*longStress;
+longStrain = P*d/(2*t*E) *(0.5-v);
+circumStrain = P*d/(2*t*E) *(1-0.5*v);
+VolStrain = P*d/(t*E) *(5/4 -v); %volume percentage increase
+
+%Bulkheads
+%Rear Bulkhead, assuming its spherical
+RBlkStress = P*r/(2*t);
+
+%front bulkhead
+
+dzdx = (-b/a)*(a^2-x^2)^(-0.5)*x;
+
+
+%FBlkStress = P*raa/(2*t*cos(a));
